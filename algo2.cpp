00548cac91ccec91314fe59d1188e9c964a31934
libra guest <adrien.lardenois@gmail.com>
2012-10-05 05:01:48 -0400

    ajout de algo 2, need test

---
+++ algo2.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <cstring>
+#include <String>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+
+int algo2 ( char*& S, char*& T, int& taille){
+ 
+    int max = 0;
+    int l , i , j ;
+    
+    for(l=1; l <= taille ; l++){
+        for( i=1 ; i+l-1 <= taille ; i++){
+            for( j=1 ; j+l-1 <= taille; j++){
+                if(strncmp(&S[i] , &T[j] , l) == 0){
+                    max = l;
+                }
+            }
+        }
+    
+    }
+    return max;
+ 
+}
