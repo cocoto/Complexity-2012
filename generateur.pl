779596e5a2775d1abff51af506a245cf9c594198
cocoto <cocoto@cocoto-K52F.(none)>
2012-10-06 17:24:27 +0200

    Debug algo1, ajout générateur de chaîne prolog

---
+++ generateur.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/swipl -q -s
+%/Générateur de problèmes de taille n/
+%/Entrez la Taille n suivit d'un "." et le scipt génère en sortie standard toutes les permutation de ATCG dans n/
+go :- 
+  T = ['A','T','C','G'],
+  X=[],
+  read(Taille),
+
+ remplir(T,X,Taille);true.
+
+remplir(_,Y,0):-
+    !,forall(member(J,Y),write(J)),write('\n'),!,false.
+remplir(T,X,N) :-
+  N2 is N-1,!,
+  member(A,T),
+  append(X,[A],B),
+  remplir(T,B,N2).
+
+  
+?- go.
+?- halt.
