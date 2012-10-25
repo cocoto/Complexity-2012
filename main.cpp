8f19d94105cc439ad862fe1de9822388041395a0
cocoto <cocoto@cocoto-K52F.(none)>
2012-10-25 22:22:10 +0200

    premier jet rapport

--- main.cpp
+++ main.cpp
@@ -6,21 +6,23 @@
 #include <fstream>
 
 int main(int argc, char *argv[]){
-  int taille=0;
-  double min;
-  double max;
-  double sum;
-  //int value;
-  double time;
-  int cpt;
-  char* S=NULL;
-  char* T=NULL;
-  int (*pfonction)(char*,char*,const int &);
-  std::ifstream fichier;
-  int ligne_courante=0;
-  bool sortie;
-  int cpt_argument=2;
-  int facteur; //On répète facteur fois le même test entre le début et la fin du chronomètre, pour obtenir un temps >1ms
+  
+  //Déclaration des variables
+  
+  int taille=0; //Taille des chaînes (change à chaque paramètre)
+  double min; //Temps minimum observé
+  double max; //Temps maximum observé
+  double sum; //Temps total depuis le début de l'execution (par paramètre)
+  double time;//Temps d'une execution
+  int cpt;    //Compteur de génération
+  char* S=NULL; //Chaîne à comparer
+  char* T=NULL; //Idem
+  int (*pfonction)(char*,char*,const int &); //Pointeur de fonction (algo1,2,3)
+  std::ifstream fichier;	//Pointeur du fichier en cour de lecture (paramètre)
+  int ligne_courante=0;		//Second curseur (virtuel) dans le fichier
+  bool sortie;			//Condition de sortie de boucle en lecture de fichier
+  int cpt_argument=2;		//Compteur de l'argument en cours de lecture
+  int facteur;       		//On répète facteur fois le même test entre le début et la fin du chronomètre, pour obtenir un temps >1ms
   
   //Attribution de l'algorithme en fonction du premier paramètre (1,2,3)
   switch (atoi(argv[1])){
@@ -30,38 +32,52 @@ int main(int argc, char *argv[]){
     default : std::cout<<"Algorithm "<<atoi(argv[1])<<" does not exist !\n"; return 0;break;
   }
 
+  //On parcourt chaque argument (demande de jeux de tests)
   while(cpt_argument<argc)
 	{
+	  
+	  //INITIALISATION DES VARIABLES
 	  cpt=0;
-	  min=200000000;
-	  max=-1;
+	  min=200000000; //min doit être plus grand que 3minutes (en ms)
+	  max=-1;	//Valeur impossible (min=0)
 	  time=0;
 	  sum=0;
 	  sortie=false;
-	  //Si la taille est passée en paramètre (aléatoire)
+	  //Si la taille est passée en paramètre (génération aléatoire)
 	  if(atoi(argv[cpt_argument])!=0)
 	  {
 	    taille=atoi(argv[cpt_argument]);
-
-
-		//Génération aléatoire
-		S=random_generate(taille,cpt);
-		T=random_generate(taille,cpt+1);
-		facteur=1;
-		do{	
-			crono_start();
-			for(int i=0;i<facteur;i++)
-			{
-			 (*pfonction)(S,T,taille);
-			}
-			crono_stop();
-			//std::cout<<"facteur="<<facteur<<" crono="<<crono_ms()<<"\n"; //@DEBUG
-			if(crono_ms()<5)
-			{
-			    facteur*=10;
-			}
-		}
-		while(crono_ms()<5);
+		
+	    //Génération aléatoire
+	    S=random_generate(taille,cpt);
+	    T=random_generate(taille,cpt+1);
+	    facteur=1;
+	    
+	    /*
+	     * Boucle déterminant le facteur minimum à appliquer pour obtenir des chronométrages >1ms
+	     */
+	    do{	
+		    //Lancement du chronomètre
+		    crono_start();
+		    for(int i=0;i<facteur;i++)
+		    {
+		      //On effectue facteur fois la résolution
+		      (*pfonction)(S,T,taille);
+		    }
+		    crono_stop();
+		    //Si le temps de résolution est inférieur à 5ms, on multiplie par 10 le facteur
+		    if(crono_ms()<5)
+		    {
+			facteur*=10;
+		    }
+	    }
+	    while(crono_ms()<5);
+	    /*
+	     * Fin de la boucle de recherche de facteur
+	     */
+	    
+	    
+	    
 	    //On génère des chaînes aléatoires jusqu'à 3 minutes de temps total de résolution
 	      while(sum<180*1000)
 	      {
@@ -69,6 +85,7 @@ int main(int argc, char *argv[]){
 		S=random_generate(taille,cpt);
 		T=random_generate(taille,cpt+1);
 
+		//Lancement du chronomètre
 		crono_start();
 		for(int i=0;i<facteur;i++)
 		{
@@ -88,31 +105,45 @@ int main(int argc, char *argv[]){
 		{
 		  min=time;
 		}
-		cpt+=2;
-	
-		//Décommenter pour débug
-		//std::cout<< S <<" et "<< T <<"  =  "<< value << " en "<<time<<"ms total "<<sum/1000<<"s \n";
+		cpt+=2; //Voir fonction random_generate pour plus de détails
 	
 		//Libération de S et T (cf random_generate)
 		free(S);
 		free(T);
 	  	}
-	}
+	}//boucle d'argument===taille
+	
+	
 	  //Sinon, si un fichier est passé en paramètre
 	  else
 	  {
+	    //Ouverture du fichier
 	    fichier.open(argv[cpt_argument]);
+	    //Contrôle de l'ouverture
 	    if(fichier)
 	    {
+	      //Récupération de la taille des chaînes en première ligne
 	      fichier>>taille;
+	      
+	      
 	      S=(char*)malloc(taille*sizeof(char));
 	      S[0]=' ';
 	      T=(char*)malloc(taille*sizeof(char));
 	      T[0]=' ';
+	      
+	      //Récupération de la première chaine
 	      fichier>>&S[1];
+	      
+	      //Sauvegarde de la position (curseur virtuel)
 	      ligne_courante=fichier.tellg();
+	      
+	      //Recopie de S dans T pour la première comparaison
 	      strcpy(T,S);
 		
+	      
+	      /*
+	       * Détermination du facteur minimum
+	       */
 		facteur=1;
 		do{	
 			crono_start();
@@ -128,6 +159,9 @@ int main(int argc, char *argv[]){
 			}
 		}
 		while(crono_ms()<5);
+		
+		
+		//Tant que toutes les chaînes n'ont pas été parcourues
 		while(!sortie)
 		{
 		    //On résoud toutes les paires possibles du fichier
@@ -169,8 +203,6 @@ int main(int argc, char *argv[]){
 			min=time;
 		      }
 		      cpt+=2;
-		      //Décommenter pour débug
-		      //std::cout<< S <<" et "<< T <<"  =  "<< value << " en "<<time<<"ms total "<<sum/1000<<"s \n";
 		      fichier>>&T[1];
 		}
 		
@@ -179,15 +211,19 @@ int main(int argc, char *argv[]){
 		free(T);
 		fichier.close();
 	    } 
-	    else
+	    else //ERREUR D'OUVERTURE DE FICHIER
 	    {
 	       std::cout<<"Wrong file input !\n";
 	       return 0;
 	    }    
-	  }
+	  }//boucle d'argument===fichier
 	    
 	    //Impression des statistiques
 	    std::cout<<"******************\ntaille = "<<taille<<"\nminimum="<<min<<"ms \nmaximum="<<max<<"ms \nmoyen="<<sum/(cpt/2)<<"ms \ntotal="<<sum<<"ms \nnb_tests="<<cpt/2<<" \nfacteur="<<facteur<<"\n******************\n\n";
 	    cpt_argument++;
-	}
-}
+	
+	  
+	}//Boucle sur les arguments
+	
+	return 0;
+}//main
